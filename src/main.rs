cc813aff328f33607b4893168ab3684f0bf491dd
abhishekgurjar007 <abhidada0710@gmail.com>
2026-06-14 14:28:04 +0530

    Update gitignore and Rust code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main(){
+// Compound Data Types;
+
+// {Array, Tuplse, Slices, Strings & (Slice String)}
+
+fn main() {
     
+    //Array
+
+    let numbers:[i32; 5] = [1, 2, 3, 4, 5];
+    println!("Numbers: {:?}", numbers);
 }
