be0555ff567f12eed28a66bc039861640ba8c546
abhishekgurjar007 <abhidada0710@gmail.com>
2026-06-17 00:02:29 +0530

    Shared practice questions for topic-Primitive & Compoutnd Data Types as pdf

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 //Functions
 // fn function_name(parameters) -> return_type {
-fn main() {
+// Every finction / variable name should be written in snake_case
 
+
+fn hello_world() {
+    println!("hello, rust🦀!");
 }
+fn main() {
+    hello_world();
+}
+
